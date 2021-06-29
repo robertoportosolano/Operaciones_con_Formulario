0c1b5298e5ecb51309db7bd861b9d44395dd7cef
robertoportosolano <robertoportosolano@users.noreply.github.com>
2021-06-28 19:48:47 -0500

    login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  numero1:any;
+  numero2:any;
+  Suma:number;  
 
   constructor() {}
+  
+  
+  sumaNumero(){
+   var N1=parseInt(this.numero1);
+   var N2=parseInt(this.numero2);
+   this.Suma=N1+N2;
+   this.numero1="";
+   this.numero2="";
+  }
+
 
 }
